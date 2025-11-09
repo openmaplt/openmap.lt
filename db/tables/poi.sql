eb45ea66879d3e127b992bcc44265d10b4a6cd2d
tomass <tomasstraupis@gmail.com>
2025-11-09 17:11:14 +0200

    Pridėti laukai, kurių trūko POI pokyčiams apdoroti

--- db/tables/poi.sql
+++ db/tables/poi.sql
@@ -1,14 +1,16 @@
 create table places.poi (
   id serial primary key
- ,osm_type char
+ ,obj_type char
  ,osm_id bigint
  ,attr jsonb
+ ,x char
  ,geom geometry(point,3857)
 );
 
 comment on column places.poi.id is 'Primary key';
-comment on column places.poi.osm_type is 'Object type: n - node, w - way, r - relation';
+comment on column places.poi.obj_type is 'Object type: n - node, w - way, r - relation';
 comment on column places.poi.osm_id is 'Open street map object id';
 comment on column places.poi.attr is 'Array of ordered place attributes, formatted for displaying';
+comment on column places.poi.x is 'Column is used during POI update, it helps find POIs which no longer exist in OSM';
 
 create index poi_gix on places.poi using gist(geom);
