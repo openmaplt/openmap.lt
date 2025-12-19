ec38758cf9b363c328b1c777d02f7f506581e16e
tomass <tomasstraupis@gmail.com>
2025-12-19 19:29:21 +0200

    Įdiegti funkciją

--- docker/db/plsql/install_plsql.sql
+++ docker/db/plsql/install_plsql.sql
@@ -1,3 +1,4 @@
 \i /docker-entrypoint-initdb.d/plsql/list.pls
 \i /docker-entrypoint-initdb.d/plsql/search.pls
 \i /docker-entrypoint-initdb.d/plsql/get_where_condition.pls
+\i /docker-entrypoint-initdb.d/plsql/poi_info.pls
